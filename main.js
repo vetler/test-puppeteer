b231372e0e7af957c0556d8bcde2103a75fa6eda
Vetle Leinonen-Roeim <vetle@reprice.no>
2022-10-25 12:54:38 +0200

    Only lowercase characters in function name

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-exports.helloWorld = async (req, res) => {
+exports.helloworld = async (req, res) => {
     const browser = await puppeteer.launch({headless: true});
     await browser.close();
     res.send('Hello, World!');
